695d1280faa7284bae57378a73171d281c89dd14
mrhulo-tech <info@hulotech.com>
2026-04-22 18:00:48 -0400

    Add skill card component

---
+++ frontend/components/SkillCard.tsx
@@ -0,0 +1,12 @@
+export default function SkillCard() {
+  return (
+    <section>
+      <h2>Compétences observées</h2>
+      <ul>
+        <li>Expression orale</li>
+        <li>Compréhension</li>
+        <li>Interaction</li>
+      </ul>
+    </section>
+  );
+}

a3b326df9dca464d2499e7994eeaa260b0972a0d
mrhulo-tech <info@hulotech.com>
2026-04-22 17:37:42 -0400

    Add recommendation list component

---
+++ frontend/components/RecommendationList.tsx
@@ -0,0 +1,11 @@
+export default function RecommendationList() {
+  return (
+    <section>
+      <h2>Recommandations</h2>
+      <ul>
+        <li>Pratique orale guidée</li>
+        <li>Exercice de reformulation</li>
+      </ul>
+    </section>
+  );
+}

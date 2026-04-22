4c7c3cc948dfb4b6cfcf7eb1f552db401f2711b5
mrhulo-tech <info@hulotech.com>
2026-04-22 17:37:12 -0400

    Add indicator card component

---
+++ frontend/components/IndicatorCard.tsx
@@ -0,0 +1,11 @@
+export default function IndicatorCard() {
+  return (
+    <section>
+      <h2>Indicateurs</h2>
+      <ul>
+        <li>Compréhension orale : 78</li>
+        <li>Expression orale : 54</li>
+      </ul>
+    </section>
+  );
+}

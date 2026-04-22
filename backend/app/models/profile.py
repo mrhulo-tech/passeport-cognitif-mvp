59d033e6982cbfe26795bed5488db11246fe5ae1
mrhulo-tech <info@hulotech.com>
2026-04-22 17:24:26 -0400

    Add profile model

---
+++ backend/app/models/profile.py
@@ -0,0 +1,6 @@
+class Profile:
+    def __init__(self, user_id, current_level, target_level, progress_score):
+        self.user_id = user_id
+        self.current_level = current_level
+        self.target_level = target_level
+        self.progress_score = progress_score

9b34d9d7add5c9fe1d9fcbd1ef81529c16e9dac7
mrhulo-tech <info@hulotech.com>
2026-04-22 17:47:01 -0400

    Add skill snapshot model

---
+++ backend/app/models/skill_snapshot.py
@@ -0,0 +1,6 @@
+class SkillSnapshot:
+    def __init__(self, user_id, skill_name, score, timestamp):
+        self.user_id = user_id
+        self.skill_name = skill_name
+        self.score = score
+        self.timestamp = timestamp

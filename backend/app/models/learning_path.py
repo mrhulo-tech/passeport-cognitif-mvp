b1256c1d381b5ff0c46d1a9b6ffef8b766fd6c86
mrhulo-tech <info@hulotech.com>
2026-04-22 17:46:01 -0400

    Add learning path model

---
+++ backend/app/models/learning_path.py
@@ -0,0 +1,6 @@
+class LearningPath:
+    def __init__(self, user_id, recommended_module, priority, rationale):
+        self.user_id = user_id
+        self.recommended_module = recommended_module
+        self.priority = priority
+        self.rationale = rationale

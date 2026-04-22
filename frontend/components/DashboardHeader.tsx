7f57671e076861a49fb9682bb9dbb2ac08b45534
mrhulo-tech <info@hulotech.com>
2026-04-22 17:59:51 -0400

    Add dashboard header component

---
+++ frontend/components/DashboardHeader.tsx
@@ -0,0 +1,8 @@
+export default function DashboardHeader() {
+  return (
+    <header>
+      <h1>Passeport Cognitif – Tableau de bord</h1>
+      <p>Profil d’apprentissage et progression</p>
+    </header>
+  );
+}

87e6fb2e27c40b7521763ea208a88a7a38fb23c1
mrhulo-tech <info@hulotech.com>
2026-04-22 17:38:25 -0400

    Add progress timeline component

---
+++ frontend/components/ProgressTimeline.tsx
@@ -0,0 +1,12 @@
+export default function ProgressTimeline() {
+  return (
+    <section>
+      <h2>Progression</h2>
+      <ul>
+        <li>S1 : Diagnostic</li>
+        <li>S2 : Activités ciblées</li>
+        <li>S3 : Nouvelle tâche orale</li>
+      </ul>
+    </section>
+  );
+}

329fb63d1c1d0166d139753bedd2d5594a09407a
mrhulo-tech <info@hulotech.com>
2026-04-22 18:14:38 -0400

    Use app name in dashboard header

--- frontend/components/DashboardHeader.tsx
+++ frontend/components/DashboardHeader.tsx
@@ -1,7 +1,9 @@
+import { APP_NAME } from "../lib/constants";
+
 export default function DashboardHeader() {
   return (
     <header>
-      <h1>Passeport Cognitif – Tableau de bord</h1>
+      <h1>{APP_NAME}</h1>
       <p>Profil d’apprentissage et progression</p>
     </header>
   );
